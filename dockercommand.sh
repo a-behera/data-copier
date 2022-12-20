83d3ce42b1f8aaf93d869ab5a284560627ae9385
a-behera <ankitbehera799@gmail.com>
2022-12-20 15:52:35 +0530

    Dockerized the application

---
+++ dockercommand.sh
@@ -0,0 +1,1 @@
+docker run -it --name data-copier --rm --network data-copier-nw -v "C:\Users\Ankit Behera\OneDrive\Documents\my-projects\data-engg\research\data\retail_db_json:/retail_db_json" -e BASE_DIR=/retail_db_json -e DB_HOST=cec1e24adf7d -e DB_PORT=5432 -e DB_NAME=retail_db -e DB_USER=retail_user -e DB_PASSWORD=itversity data-copier python /data-copier/app/app.py departments,categories
